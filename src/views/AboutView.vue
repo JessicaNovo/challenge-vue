8844b42a8463abc194b0068b1b28ffed0258f646
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2023-03-06 20:58:41 +0000

    refactor(about): update about page content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,45 +1,51 @@
 <template>
-  <div class="about">
+  <main class="about">
     <h1 class="about__title blue">
-      Don't forget that this is what we are looking for 😉
+      How to use:
     </h1>
 
     <ul class="about__list">
-      <li>Nice user experience - <strong>polish and an eye for design: focus on making it look good.</strong></li>
-      <li>Clean project setup and well documented</li>
-      <li>Elegant/fast way to upload data</li>
-      <li>Ability to dive into a new topic, extract the important points and then code it up.</li>
-      <li>Make it snappy</li>
-      <li>Fulfill the exercise expectations</li>
-      <li>Write clear and well-structured code. You won’t work alone. It should be easy to understand and modify your code.</li>
-      <li>Send working instructions. Do you imagine you buy a product that you need to install by yourself and when you follow the instructions provided, it does not work? It would be frustrating.</li>
-      <li>Write clear and well-structured code. You won’t work alone. It should be easy to understand and modify your code.</li>
-      <li><strong>We expect a README file in the repository.</strong> This README must explain very clearly the instructions for us to start the application without any type of debugging or complication. The instructions must work out of the box. <strong>This is one of the most important part of the problem.</strong> You need to think always about who is going to use it and try to reduce frustration.</li>
+      <li>Start by logging in to enjoy all the features</li>
+      <li>Access your profile by clicking your name in the navigation header</li>
+      <li>On your profile page you can:</li>
+      <ul>
+        <li>See all your posts</li>
+        <li>Create a bew post, by clicking the New Post button, filling the content and saving</li>
+      </ul>
+      <li>On the Home page you can search for any user posts by entering theur email address and clicking the Search button</li>
     </ul>
-  </div>
+
+    <div class="enjoy-text blue">
+      Enjoy the app!
+    </div>
+  </main>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
-    align-items: center;
     flex-direction: column;
+    align-items: flex-start;
     justify-content: center;
   }
 }
 
-.about__title{
+.about__title {
   margin-bottom: 30px;
 }
 
-.about__list{
-  font-size: 16px;
+.about__list {
+  font-size: 24px;
+  color: var(--color-text)
 }
 
-strong {
-  font-weight: bold;
-  color: rgb(185, 185, 185);
+
+.enjoy-text {
+  width: 100%;
+  font-weight: 700;
+  font-size: 24px;
+  text-align: center;
+  margin-top: 18px;
 }
 </style>
